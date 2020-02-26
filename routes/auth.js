aee7075f3d282a8b4cac840a202f119a7d15ee9c
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-26 23:13:08 +0700

    npm bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,27 @@
 const router = require('express').Router();
 const User = require('../model/user');
-
-
-//Validate
-const Joi = require('@hapi/joi');
-const schema = Joi.object({
-    name : Joi.string()
-           .min(6)
-           .required(),
-    email : Joi.string()  
-           .min(6)
-           .required()
-           .email(),
-    password : Joi.string()
-           .min(6)
-           .required()
-})
+const {registerValidation} = require('../validate');
 
 
 router.post('/register',  async (req, res) =>{
     //Validate data before we a user
-    const validation = schema.validate(req.body)
-    res.send(validation)
-    // const user = new User({
-    //     name : req.body.name,
-    //     email : req.body.email,
-    //     password : req.body.password
-    // })
-    // try{
-    //     const savedUser = await user.save()
-    //     res.send(savedUser)
-    // }catch(err){
-    //     res.status(400).send(err)
-    // }
+    const { error } = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    
+    const user = new User({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    })
+
+    const emailExist = await User.findOne({email : req.body.email})
+    if(emailExist) return res.status(400).send("Email already exist")
+    try{
+        const savedUser = await user.save()
+        res.send(savedUser)
+    }catch(err){
+        res.status(400).send(err)
+    }
 })
 
 module.exports = router
