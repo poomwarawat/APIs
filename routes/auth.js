61903ae1b283a27c36059325bd944292ffa61462
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-26 22:19:23 +0700

    validate

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,38 @@
 const router = require('express').Router();
 const User = require('../model/user');
 
+
+//Validate
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    name : Joi.string()
+           .min(6)
+           .required(),
+    email : Joi.string()  
+           .min(6)
+           .required()
+           .email(),
+    password : Joi.string()
+           .min(6)
+           .required()
+})
+
+
 router.post('/register',  async (req, res) =>{
-    const user = new User({
-        name : req.body.name,
-        email : req.body.email,
-        password : req.body.password
-    })
-    try{
-        const savedUser = await user.save()
-        res.send(savedUser)
-    }catch(err){
-        res.status(400).send(err)
-    }
+    //Validate data before we a user
+    const validation = schema.validate(req.body)
+    res.send(validation)
+    // const user = new User({
+    //     name : req.body.name,
+    //     email : req.body.email,
+    //     password : req.body.password
+    // })
+    // try{
+    //     const savedUser = await user.save()
+    //     res.send(savedUser)
+    // }catch(err){
+    //     res.status(400).send(err)
+    // }
 })
 
 module.exports = router
